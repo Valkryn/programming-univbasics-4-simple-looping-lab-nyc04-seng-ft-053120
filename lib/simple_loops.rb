4af05349a76d5d12cad0346afce361554a9de4f3
Calvin Gutierrez <calvinraphaelg@gmail.com>
2020-06-03 17:47:38 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -23,3 +23,13 @@ def output_array(message)
     counter += 1
   end
 end
+
+def return_string_array(array)
+  counter = 0
+  new_array = []
+  while counter < array.length
+    new_array << array(counter).to_s
+    counter += 1
+  end
+  return new_array  
+end
